515916f4237d2ce36f06dac5f91810916bcbc8b5
ice <iceloveboa@163.com>
2016-11-01 23:13:14 +0800

    by ice

---
+++ ccompiler/ccompiler/testc.c
@@ -0,0 +1,14 @@
+//
+//  testc.c
+//  ccompiler
+//
+//  Created by ice on 01/11/2016.
+//  Copyright 2016 ice. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(){
+    printf("Hello world\n");
+    return 0;
+}
